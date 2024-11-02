7e96a40f51d9df6a54f2940adab755aa866f5f15
Raza Haider <razah12145@gmail.com>
2024-11-02 16:15:40 +0500

    image loading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = ({ list }) => {
-  return (list ?? [])?.map((item) => <div>{item.title}</div>);
+  return (list ?? [])?.map((item) => <img src={item.image} alt={item.title} width={100} height={100} />);
 };
 
 export default App;

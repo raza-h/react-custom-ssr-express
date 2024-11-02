2fbbcf3a372445a02aecc30ded9224c994180498
Raza Haider <razah12145@gmail.com>
2024-11-02 14:48:47 +0500

    server simplified

--- server/server.js
+++ server/server.js
@@ -6,28 +6,10 @@ import App from "../src/App";
 const app = express();
 
 app.get("/", async (req, res) => {
-
   const response = await fetch("https://fakestoreapi.com/products");
-  const products = await response.json(); 
-
-  const entryPoint = ["/main.js"];
-
-  const { pipe, abort: _abort } = ReactDOMServer.renderToPipeableStream(
-      <App list={products} />
-    ,
-    {
-      bootstrapScripts: entryPoint,
-      onShellReady() {
-        res.statusCode = 200;
-        res.setHeader("Content-type", "text/html");
-        pipe(res);
-      },
-      onShellError() {
-        res.statusCode = 500;
-        res.send("<!doctype html><p>Loading...</p>");
-      },
-    }
-  );
+  const products = await response.json();
+  const html = ReactDOMServer.renderToString(<App list={products} />);
+  res.send(`<!doctype html>${html}`);
 });
 
 app.listen(3002, () => {
